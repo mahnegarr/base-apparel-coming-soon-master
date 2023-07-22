50d2be3e371692921f5082f7aa9867ff183e313a
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-07-22 12:18:04 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const submit = document.querySelector(".submit");
+const form = document.querySelector("form")
+const error = document.querySelector(".error-msg");
+const errorIcon = document.querySelector(".error-icon");
+const input = document.getElementById("email");
+const inputEmail = document.querySelector(".input-email");
+
+// form.addEventListener("submit", validated)
+// submit.addEventListener("click", validated)
+
+
+function validated(e){
+    
+    let inputValue = input.value
+    console.log(inputValue);
+}
